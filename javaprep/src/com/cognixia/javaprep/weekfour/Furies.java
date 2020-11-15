aeee587b4a3331a3458eacd2db12cdf4e1cace6a
MakarTch <65026668+MakarTch@users.noreply.github.com>
2020-11-14 23:52:34 -0500

    interview questions

--- javaprep/src/com/cognixia/javaprep/weekfour/Furies.java
+++ javaprep/src/com/cognixia/javaprep/weekfour/Furies.java
@@ -1,9 +1,29 @@
 package com.cognixia.javaprep.weekfour;
 
+import java.util.*;
+
 public class Furies {
 
 	public static void main(String[] args) {
-		//Aii I want to do collections rn
+		//Collections
+		
+		//lists
+		ArrayList<Integer> arrList = new ArrayList<>();
+		arrList.add(1);
+		System.out.println(arrList);
+		
+		LinkedList<Integer> linkList = new LinkedList<>();
+		//Whats the difference between an arraylist and a linkedlist?
+		/*
+		 * linkedlist is fast for adding and delting elements, but slow to access an element
+		 * arraylist is fast for accessing a specific element but slow to add or delete.
+		 */
+		
+		//
+		int [] arr = {4};
+		arr[0] = 2;
+		System.out.println(arr[0]);
+		System.out.println(arr);
 		
 	}
 
